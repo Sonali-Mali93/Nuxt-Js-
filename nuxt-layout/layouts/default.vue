011c8dfa6110983bc6f5ec5165c50da9b78c2c93
Sonali Mali <sonali.mali@olacabs.com>
2022-07-26 17:17:20 +0530

    nustlink tag use to navigate navigation bar items

---
+++ nuxt-layout/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+<div>
+    <nav>Nav goes here</nav>
+    <ul>
+    <li><NuxtLink to="/">Home page</NuxtLink></li>
+    <li><NuxtLink to="/about">Contact</NuxtLink></li>
+    <li><NuxtLink to="/contact"></NuxtLink></li>
+</ul>
+</div>
+</template>
